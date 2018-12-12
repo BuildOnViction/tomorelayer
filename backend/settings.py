28419fe5ffef527f2a252dc95c2e28b29d649e62
vutran1710 <me@vutr.io>
2018-12-12 16:10:32 +0700

    config DB Connection

--- backend/settings.py
+++ backend/settings.py
@@ -1,22 +1,32 @@
-from os import getenv, path
+from os import getenv
+from os import path
 from pathlib import Path
 from dotenv import load_dotenv
+from peewee_async import Manager
+from peewee_async import PooledMySQLDatabase
 
+# IMPORT ENVIRONMENT VARIABLES
 env_path = Path('..') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+# SETUP ASYNC ORM
+envars = ['user', 'password', 'host', 'port']
+db_name = getenv('DB_NAME')
+db_config = {k: getenv('DB_' + k.upper()) for k in envars}
+db_config['port'] = int(db_config['port'])
+
+database = PooledMySQLDatabase(db_name, **db_config)
+objects = Manager(database)
+
+# APPLICATION BACKEND SETTINGS
 settings = {
-    "template_path": path.dirname(path.abspath(__file__)) + '/template',
-    "login_url": "/login",
-    "xsrf_cookies": True,
-    "autoreload": True,
-    "port": getenv('APP_PORT'),
-    "stg": getenv('STG'),
-    "db": {
-        "name": getenv('DB_NAME'),
-        "user": getenv('DB_USER'),
-        "password": getenv('DB_PASSWORD'),
-        "host": getenv('DB_HOST'),
-        "port": getenv('DB_PORT'),
-    }
+    'autoreload': True,
+    'db': {'name': db_name, **db_config},
+    'debug': True,
+    'login_url': '/login',
+    'objects': objects,
+    'port': getenv('APP_PORT'),
+    'template_path': path.dirname(path.abspath(__file__)) + '/template',
+    'xsrf_cookies': True,
+    'stg': getenv('STG'),
 }
