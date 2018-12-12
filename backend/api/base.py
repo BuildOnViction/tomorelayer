0877b24424df73ec0d6eca994965fd8f23203fc1
vutran1710 <me@vutr.io>
2018-12-12 16:13:26 +0700

    Wiring up

--- backend/api/base.py
+++ backend/api/base.py
@@ -1,38 +1,20 @@
-import tornado.web
+from tornado.web import ErrorHandler
+from tornado.web import RequestHandler
 import json
 import traceback
-import logging
-import template
-
-logger = logging.getLogger()
-handler = logging.FileHandler('error.log')
-formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
 
 
-class BaseHandler(tornado.web.RequestHandler):
-    """
-    Base handler gonna to be used instead of RequestHandler
-    """
-
+class BaseHandler(RequestHandler):
     def write_error(self, status_code, **kwargs):
         self.set_header('Content-Type', 'application/json')
-        traceback.print_tb(traceback.print_tb(kwargs['exc_info'][2]))
-
-        if not self._reason:
-            logger.debug(traceback.format_exception(kwargs['exc_info'][1]))
-
-        error = {
-            'code': status_code,
-            'message': self._reason,
-        }
-
+        _, http_exception, stack_trace = kwargs['exc_info']
+        traceback.print_tb(stack_trace)
+        # Log the unreasoned errors
+        error = {'code': status_code, 'message': self._reason}
         self.finish(json.dumps({'error': error}))
 
 
-class ErrorHandler(tornado.web.ErrorHandler, BaseHandler):
+class ErrorHandler(ErrorHandler, BaseHandler):
     """
     Default handler gonna to be used in case of 404 error
     """
--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+from tornado.platform.asyncio import AsyncIOMainLoop
 from tornado.ioloop import IOLoop
 from tornado.web import Application
 from route import route
@@ -5,6 +6,8 @@ from settings import settings
 
 
 if __name__ == "__main__":
+    AsyncIOMainLoop().install()
     app = Application(route, **settings)
     app.listen(settings['port'])
+    app.objects = settings['objects']
     IOLoop.current().start()
