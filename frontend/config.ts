72babf3ac353e3783f688d303d45146394d3ac56
vutran1710 <me@vutr.io>
2018-12-14 11:47:08 +0700

    make ramda alias

--- frontend/config.ts
+++ frontend/config.ts
@@ -16,6 +16,7 @@ export default function fuseConfig(isProduction = false) {
     allowSyntheticDefaultImports: true,
     autoImport: {
       React: "react",
+      r: "ramda",
     },
     log: {
       showBundledFiles: false,
