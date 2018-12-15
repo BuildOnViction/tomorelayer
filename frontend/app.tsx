34414bb9ab6d8bee5283648b533f45da3fafb267
vutran1710 <me@vutr.io>
2018-12-15 15:30:55 +0700

    break line

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -8,10 +8,17 @@ import Private from './component/util/Private'
 import SampleComponent from './component/SampleComponent'
 import Welcome from './component/Welcome'
 
-import '@atlaskit/css-reset/dist/bundle.css'
 import './style/app.scss'
 
-const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rutrum nisl ut ante dignissim, at tempor elit pellentesque. Donec cursus semper arcu semper vestibulum. Morbi et nibh quis nulla mollis vehicula nec luctus nulla. In eleifend rhoncus sagittis. Donec non odio vel neque laoreet aliquet vel quis dui. Duis id metus nisl. Aliquam a est in neque maximus maximus. Maecenas fringilla nibh porta libero eleifend, quis tempus leo auctor. Vestibulum non felis vel nibh laoreet semper vel eu nisi.'
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Nunc rutrum nisl ut ante dignissim, at tempor elit pellentesque.
+Donec cursus semper arcu semper vestibulum.
+Morbi et nibh quis nulla mollis vehicula nec luctus nulla. In eleifend rhoncus sagittis.
+Donec non odio vel neque laoreet aliquet vel quis dui. Duis id metus nisl.
+Aliquam a est in neque maximus maximus.
+Maecenas fringilla nibh porta libero eleifend, quis tempus leo auctor.
+Vestibulum non felis vel nibh laoreet semper vel eu nisi.
+`
 
 const InnerContent = styled.div`
   padding: 2em;
