38add9adad98af05e507f074ffd06a364e0abc63
vutran1710 <me@vutr.io>
2018-12-17 10:24:57 +0700

    Add favicon

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -11,7 +11,21 @@ const react = [
   '@neutrinojs/react',
   {
     html: {
-      title: 'Epic React App'
+      title: 'Epic React App',
+      links: [
+        'https://fonts.googleapis.com/css?family=Roboto',
+        {
+          href: '/static/apple-touch-icon.png',
+          rel: 'apple-touch-icon',
+          sizes: '180x180'
+        },
+        {
+          href: '/static/favicon.ico',
+          rel: 'icon',
+          sizes: '32x32',
+          type: 'image/png'
+        }
+      ],
     },
     devServer: {
       port: 3000
