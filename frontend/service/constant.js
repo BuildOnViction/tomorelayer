f10f441b716da34ed22149e579f6ca78739ec02d
vutran1710 <me@vutr.io>
2018-12-17 14:19:45 +0700

    Add rules

--- frontend/service/constant.js
+++ frontend/service/constant.js
@@ -1,5 +1,4 @@
 export const baseUrl = process.env.STG === 'production' ? 'https://.../' : `http://www.localhost:${process.env.APP_PORT}`
-console.log(baseUrl)
 
 export const SITE_MAP = {
   root: '/',
