c0893898240e3f8d81e5045a3bef9fff4c726d7f
vutran1710 <me@vutr.io>
2019-01-03 10:07:28 +0700

    use HashRouter for development

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route, HashRouter as Router, Switch } from 'react-router-dom'
 import { SITE_MAP } from '@constant'
 import Home from '@route/home'
 import Dashboard from '@route/dashboard'
