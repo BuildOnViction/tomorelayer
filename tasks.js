3f4aaf620480b42477a9e55f8684baba4192207f
vutran1710 <me@vutr.io>
2019-01-09 11:04:09 +0700

    modify run script

--- tasks.js
+++ tasks.js
@@ -1,17 +1,19 @@
 const sh = require('shelljs')
 const clk = require('chalk')
+const sig = require('signale')
 const { task } = require('fuse-box/sparky')
 
-// Pre-run config
+
+// PRE-RUN CONFIG
 require('dotenv').config()
-require('pretty-error').start()
-// const pe = new PrettyError()
 
-// Collecting command-line arguments
+
+// COLLECTING COMMAND-LINE ARGUMENTS
 const args = []
-process.argv.forEach((val, index, array) => args.push(val))
+process.argv.forEach(arg => args.push(arg))
 
-// Scanning...
+
+// ENVIRONMENT VARIABLES
 const {
   DB_NAME,
   DB_USER,
@@ -22,21 +24,28 @@ const {
 
 const database = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
-// Yahoooooooooo...
+
+// BASIC STUFFS
 task('fe', () => sh.exec('node -r dotenv/config -r ts-node/register --inspect frontend/fuse.ts'))
 task('be', () => sh.exec('pipenv run python ./backend/app.py'))
 task('dock', () => sh.exec('docker-compose -f docker-compose.dev.yaml up -d'))
 task('embark:compile', () => sh.exec('embark run --nobrowser --noserver'))
 
+
 /*
- * Database is a liiiiiiiiiilttle more complicated
+ * Database is more complicated
  * we need to write migration script ourselves, instead of Django Magic...
  * and, we need to run migrate command afterwards
  */
 task('db:create', () => {
   const mig_name_fmt = clk.green.bold('Migration_Name')
   const migrationName = args[3]
-  const ArgsError = new Error(`Missing ${mig_name_fmt}. Passing an argument as ${mig_name_fmt}`)
-  const cmd = `pipenv run pw_migrate create --auto-source=backend --database=${database} --verbose ${migrationName}`
-  return migrationName ? sh.exec(cmd) : console.log(ArgsError)
+  const ArgsError = () => sig.fatal(`- Missing ${mig_name_fmt}. Passing an argument as ${mig_name_fmt}`)
+  const cmd = `pipenv run pw_migrate create --auto --database=${database} --verbose ${migrationName}`
+  return migrationName ? sh.exec(cmd) : ArgsError()
+})
+
+task('db:migrate', () => {
+  const cmd = `pipenv run pw_migrate migrate --database=${database} --verbose`
+  return sh.exec(cmd)
 })
