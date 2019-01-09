97afa1507b008121fce23ee1e2caf28fa44a7310
vutran1710 <me@vutr.io>
2019-01-09 14:22:40 +0700

    Add default_handler for 404

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,12 @@ from tornado.ioloop import IOLoop
 from tornado.web import Application
 from route import route
 from settings import settings
+from api.notfound import NotFoundHandler
 
 
 if __name__ == "__main__":
     AsyncIOMainLoop().install()
-    app = Application(route, **settings)
+    app = Application(route, default_handler_class=NotFoundHandler, **settings)
     app.listen(settings['port'])
     app.objects = settings['objects']
     IOLoop.current().start()
