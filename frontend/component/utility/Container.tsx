c7ba9ff1bdba6ee5170edc5f93506ebc38057be9
vutran1710 <me@vutr.io>
2019-01-10 13:13:23 +0700

    move util component to separate folder

---
+++ frontend/component/utility/Container.tsx
@@ -0,0 +1,7 @@
+const Container = ({ className, children }) => (
+  <div className={`container ${className}`}>
+    {children}
+  </div>
+)
+
+export default Container
---
+++ frontend/component/utility/index.ts
@@ -0,0 +1,9 @@
+import Container from './Container'
+import Divider from './Divider'
+import Private from './Private'
+
+export {
+  Container,
+  Divider,
+  Private,
+}
