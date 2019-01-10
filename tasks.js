1c49631a825b369f419a453bfe907dbf985d2bbf
vutran1710 <me@vutr.io>
2019-01-10 10:01:20 +0700

    fix script to run backend

--- tasks.js
+++ tasks.js
@@ -27,9 +27,11 @@ const database = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/$
 
 // BASIC STUFFS
 task('fe', () => sh.exec('node -r dotenv/config -r ts-node/register --inspect frontend/fuse.ts'))
-task('be', () => sh.exec('pipenv run python ./backend/app.py'))
 task('dock', () => sh.exec('docker-compose -f docker-compose.dev.yaml up -d'))
 task('embark:compile', () => sh.exec('embark run --nobrowser --noserver'))
+task('embark:migrate', () => {
+  // for any schema-alteration scripts
+})
 
 
 /*
