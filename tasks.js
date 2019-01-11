236ffb6037a9c1a2222f033eb643982ffc4e6da8
vutran1710 <me@vutr.io>
2019-01-11 18:23:36 +0700

    Fix script

--- tasks.js
+++ tasks.js
@@ -26,7 +26,6 @@ const database = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/$
 
 
 // BASIC STUFFS
-task('fe', () => sh.exec('node -r dotenv/config -r ts-node/register --inspect frontend/fuse.ts'))
 task('dock', () => sh.exec('docker-compose -f docker-compose.dev.yaml up -d'))
 task('embark:compile', () => sh.exec('embark run --nobrowser --noserver'))
 task('embark:migrate', () => {
