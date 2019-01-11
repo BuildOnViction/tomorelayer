96b7608df8b8febb5f0bd80486b850c7ab2d96ea
vutran1710 <me@vutr.io>
2019-01-11 11:24:28 +0700

    Add grid.css

--- frontend/component/utility/Container.tsx
+++ frontend/component/utility/Container.tsx
@@ -1,7 +1,17 @@
-const Container = ({ className, children }) => (
-  <div className={`container ${className}`}>
-    {children}
-  </div>
-)
+const Container = ({
+  full = false,
+  grid = false,
+  className = '',
+  children,
+}) => {
+  const isGrid = grid ? ' grid' : ''
+  const containerType = full ? 'container-full' : 'container'
+  const fullclass = containerType + isGrid + ' ' + className
+  return (
+    <div className={fullclass} >
+      {children}
+    </div>
+  )
+}
 
 export default Container
