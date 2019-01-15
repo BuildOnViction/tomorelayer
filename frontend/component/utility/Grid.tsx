cdf21b560741566b8dfb32b02c84d603a59ffba9
vutran1710 <me@vutr.io>
2019-01-15 14:41:08 +0700

    Update css

---
+++ frontend/component/utility/Grid.tsx
@@ -0,0 +1,15 @@
+import cx from 'classnames'
+
+const Grid = ({
+  className = '',
+  children
+}) => {
+  const cls = cx('grid', className)
+  return (
+    <div className={cls}>
+      {children}
+    </div>
+  )
+}
+
+export default Grid
