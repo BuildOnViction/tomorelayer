b40ba2ff5a7ff8ae4a0961f9a6e21efd5b116843
vutran1710 <me@vutr.io>
2019-01-17 15:44:46 +0700

    Add dex_rate to Relayer Model

--- backend/api/registration.py
+++ backend/api/registration.py
@@ -8,7 +8,7 @@ class RegisterHandler(BaseHandler):
     async def post(self):
         missing_fields = []
 
-        for field in ['name', 'address', 'logo']:
+        for field in ['name', 'address', 'logo', 'dex_rate']:
             not self.request_body.get(field) and missing_fields.append(field)
 
         if missing_fields:
@@ -17,12 +17,21 @@ class RegisterHandler(BaseHandler):
         name = self.request_body['name']
         address = self.request_body['address']
         logo = self.request_body['logo']
+        dex_rate = self.request_body['dex_rate']
 
         async with self.application.objects.atomic():
-            rl = await self.application.objects.create(Relayer, name=name, address=address, logo=logo)
+            rl = await self.application.objects.create(
+                Relayer,
+                name=name,
+                address=address,
+                logo=logo,
+                dex_rate=dex_rate,
+            )
+
             self.json_response({
                 'id': rl.id,
                 'name': rl.name,
                 'address': rl.address,
+                'dex_rate': rl.dex_rate,
                 'logo': rl.logo,
             })
