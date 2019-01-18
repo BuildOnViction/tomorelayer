132f11a683d9760d28d2c71a5d99222efb383480
vutran1710 <me@vutr.io>
2019-01-18 11:27:57 +0700

    ditch wretch, go back to fetch

--- frontend/service/action.ts
+++ frontend/service/action.ts
@@ -1,25 +1,30 @@
-import wretch from 'wretch'
 import { web3 } from '@blockchain'
 import { API, ERROR, ALERT } from '@constant'
 
-
-wretch().defaults({
-  headers: {
-    'Accept': 'application/json; charset=UTF-8',
-  },
-  mode: 'no-cors'
-})
 
 export const AsynCatch = promise => promise
   .then(data => [null, data])
   .catch(err => [err])
 
+export const client = {
+  get: api => fetch(api).then(r => r.json()),
+  post: (api, value) => fetch(api, {
+    method: 'POST',
+    body: JSON.stringify(value),
+    headers: {
+      'Accept': 'application/json; charset=UTF-8',
+    },
+  }).then(r => r.json())
+}
+
 
 export const AppInitializer = ({ setState }) => ({
 
-  fetchRegisteredRelayers: () => wretch(API.relayers).get()
-    .badRequest(console.error)
-    .json(resp => setState({ relayers: resp.payload })),
+  fetchRegisteredRelayers: async () => {
+    const resp = await client.get(API.relayers)
+    if (resp.error) return { alert: resp.error.message }
+    return { alert: resp.payload, relayers: resp.payload }
+  },
 
   detectWeb3User: async () => {
     if (!web3) return setState({ alert: ALERT.web3.meta_mask_unavailable })
@@ -33,10 +38,15 @@ export const AppInitializer = ({ setState }) => ({
 
 export const RelayerRegistration = ({ setState }) => ({
 
-  registerRelayer: (state, values) => {
-    wretch(API.register).post(values)
-      .badRequest(r => console.log(r))
-      .json(r => setState({ relayers: [...state.relayers, r] }))
+  registerRelayer: async (state, values) => {
+    const resp = await client.post(API.register, values)
+    if (resp.error) return {
+      alert: resp.error.message + ':' + resp.error.detail
+    }
+    return {
+      alert: resp.payload,
+      relayers: [...state.relayers, resp.payload]
+    }
   },
 
   resetAlert: () => setState({ alert: '' }),
