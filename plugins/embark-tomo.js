0be3c94ea1c2c5bdf71d1f6c8fc2807163bce258
vutran1710 <me@vutr.io>
2019-01-19 16:18:27 +0700

    Add custom embark-tomo-plugin

---
+++ plugins/embark-tomo.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+
+module.exports = function(embark) {
+  // NOTE: we save the addresses of deployed contract to external json file
+  // so Tornando can load them on start
+
+  let contracts = []
+  let file = 'contracts.json'
+
+  const info = embark.logger.info
+
+  embark.events.on('deploy:beforeAll', () => {
+    info('>> Clear the existing ${file}')
+    fs.writeFile(file, '', 'utf8', err => {
+      if (err) throw err
+      info(`${file} created!`)
+    })
+  })
+
+  embark.events.on('deploy:contract:deployed', contract => {
+    const name = contract.className
+    const address = contract.deployedAddress
+    contracts.push({ name, address })
+  })
+
+  embark.events.on('contractsDeployed', () => {
+    info(">> All contracts deployed!")
+    try {
+      fs.appendFileSync(file, JSON.stringify(contracts))
+      info('[ok] The data was appended to file!')
+    } catch (err) {
+      info(`[x] Cannot save to ${file}`)
+    }
+  })
+
+}
