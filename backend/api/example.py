f5b6669666d615da8b4bd3810851d146ee98867b
vutran1710 <me@vutr.io>
2019-01-22 00:35:54 +0700

    Add an example for SmartContract interaction at Backend side

--- backend/api/example.py
+++ backend/api/example.py
@@ -4,65 +4,67 @@ from .base import BaseHandler
 from exception import *
 
 
-# class TestHandler(BaseHandler):
-#     def get(self):
-#         msg = 'YOLO'
-#         code = 401
-#         raise HTTPError(status_code=code, reason=msg)
+class TestHandler(BaseHandler):
+    def get(self):
+        msg = 'YOLO'
+        code = 401
+        raise HTTPError(status_code=code, reason=msg)
 
 
-# class AsyncHandler(BaseHandler):
-#     async def post(self):
-#         name = self.get_argument('name')
-#         obj = await self.application.objects.create(TestNameModel, name=name)
-#         self.write({
-#             'id': obj.id,
-#             'name': obj.name
-#         })
+class AsyncHandler(BaseHandler):
+    async def post(self):
+        name = self.get_argument('name')
+        obj = await self.application.objects.create(TestNameModel, name=name)
+        self.write({
+            'id': obj.id,
+            'name': obj.name
+        })
 
-#     async def get(self):
-#         obj_id = self.get_argument('id', None)
+    async def get(self):
+        obj_id = self.get_argument('id', None)
 
-#         if not obj_id:
-#             self.write("Please provide the 'id' query argument, i.e. ?id=1")
-#             return
+        if not obj_id:
+            self.write("Please provide the 'id' query argument, i.e. ?id=1")
+            return
 
-#         try:
-#             obj = await self.application.objects.get(TestNameModel, id=obj_id)
-#             self.write({
-#                 'id': obj.id,
-#                 'name': obj.name,
-#             })
-#         except TestNameModel.DoesNotExist:
-#             raise HTTPError(404, "Object not found!")
+        try:
+            obj = await self.application.objects.get(TestNameModel, id=obj_id)
+            self.write({
+                'id': obj.id,
+                'name': obj.name,
+            })
+        except TestNameModel.DoesNotExist:
+            raise HTTPError(404, "Object not found!")
 
 
 class TomoHandler(BaseHandler):
     """Documentation for TomoHandler
     Testing's Blockchain interaction of Tornado
+    Temporarily remove Registration contract
+    We use Test contract only
     """
-    async def post(self):
-        missing_fields = []
-
-        for field in ['name', 'address', 'logo', 'dex_rate']:
-            not self.request_body.get(field) and missing_fields.append(field)
-
-        if missing_fields:
-            raise MissingArgumentException(', '.join(missing_fields))
-
-        name = self.request_body['name']
-        address = self.request_body['address']
-        logo = self.request_body['logo']
-        dex_rate = self.request_body['dex_rate']
-
+    def prepare_contract(self):
         web3 = self.application.blockchain.web3
-        contract_data = self.application.blockchain.contracts
+        web3.eth.defaultAccount = web3.eth.accounts[0]
+        web3 = self.application.blockchain.web3
+        storevar = self.application.blockchain.contracts[0]
 
-        if dex_rate >= 1:
-            raise InvalidValueException('Dex_rate must be less than 1')
+        storevar_contract = web3.eth.contract(
+            address=storevar['deployedAddress'],
+            abi=storevar['abiDefinition'],
+        )
+        return storevar_contract
 
-        if not web3.isAddress(address):
-            logger.error('Not valid address')
-            raise InvalidValueException('Invalid address')
+    async def post(self):
+        var = self.request_body.get('var')
+        web3 = self.application.blockchain.web3
+        contract = self.prepare_contract()
+        tx_hash = contract.functions.setVar(var).transact()
+        web3.eth.waitForTransactionReceipt(tx_hash)
+        logger.warn(tx_hash)
+        self.json_response({'tx_hash': str(tx_hash)})
 
-        self.json_response({ 'success': True })
+    async def get(self):
+        contract = self.prepare_contract()
+        getvar = contract.functions.getVar().call()
+        self.json_response({'getvar': getvar})
