b234c0776578be590f046d9f8966505ff79584b8
vutran1710 <me@vutr.io>
2019-01-22 15:35:30 +0700

    init

--- tasks.js
+++ tasks.js
@@ -1,19 +1,14 @@
 const sh = require('shelljs')
 const clk = require('chalk')
 const sig = require('signale')
+const fs = require('fs')
 const { task } = require('fuse-box/sparky')
 
-
-// PRE-RUN CONFIG
 require('dotenv').config()
 
-
-// COLLECTING COMMAND-LINE ARGUMENTS
 const args = []
 process.argv.forEach(arg => args.push(arg))
 
-
-// ENVIRONMENT VARIABLES
 const {
   DB_NAME,
   DB_USER,
@@ -25,19 +20,12 @@ const {
 const database = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 
-// BASIC STUFFS
-task('dock', () => sh.exec('docker-compose -f docker-compose.dev.yaml up -d'))
-task('embark:compile', () => sh.exec('embark run --nobrowser --noserver'))
-task('embark:migrate', () => {
-  // for any schema-alteration scripts
-})
+// TASKS
+task('docker', () => sh.exec('docker-compose -f docker-compose.dev.yaml up -d'))
+task('embark:build', () => sh.exec('embark build'))
+task('')
 
 
-/*
- * Database is more complicated
- * we need to write migration script ourselves, instead of Django Magic...
- * and, we need to run migrate command afterwards
- */
 task('db:migrate', () => {
   // Reserved for future migration commmands
 })
