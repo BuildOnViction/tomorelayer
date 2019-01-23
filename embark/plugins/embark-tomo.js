df62ab54793dbe07525ec1e5d3d2d16e5c11bcbe
vutran1710 <me@vutr.io>
2019-01-23 10:29:10 +0700

    exit embark on production

--- embark/plugins/embark-tomo.js
+++ embark/plugins/embark-tomo.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
+require('dotenv').config()
 
 module.exports = function(embark) {
   // NOTE: we save the addresses of deployed contract to external json file
   // so Tornando can load them on start
 
+  const isProduction = process.env.STG === 'production'
   let contracts = []
   let file = 'contracts.json'
 
   const info = embark.logger.info
 
   embark.events.on('deploy:beforeAll', () => {
-    info('>> Clear the existing ${file}')
+    info(`========= is this production? ${isProduction}`)
+    info(`>> Clear the existing ${file}`)
     fs.writeFile(file, '', 'utf8', err => {
       if (err) throw err
       info(`${file} created!`)
@@ -26,6 +29,11 @@ module.exports = function(embark) {
     try {
       fs.appendFileSync(file, JSON.stringify(contracts))
       info('[ok] The data was appended to file!')
+
+      if (isProduction) {
+        // We terminate the dashboard after the deployment has been done!
+        process.exit(0)
+      }
     } catch (err) {
       info(`[x] Cannot save to ${file}`)
     }
