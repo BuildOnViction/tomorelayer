e88c67e966536a0c6a6d14a285a795def5455ea8
vutran1710 <me@vutr.io>
2019-01-23 19:47:48 +0700

    refactoring scripts

--- tasks.js
+++ tasks.js
@@ -1,5 +1,5 @@
 const sh = require('shelljs')
-const clk = require('chalk')
+const chalk = require('chalk')
 const sig = require('signale')
 const fs = require('fs')
 const { task } = require('fuse-box/sparky')
@@ -21,9 +21,31 @@ const database = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/$
 
 
 // TASKS
+task('kill-port', () => {
+  const killablePort = args[3]
+  if (!killablePort) {
+    sig.fatal(`Some port is required: ${killablePort}`)
+    process.exit(1)
+  }
+  sh.exec(`kill-port ${killablePort}`)
+})
+
 task('docker', () => sh.exec('docker-compose -f docker-compose.dev.yaml up -d'))
-task('embark:build', () => sh.exec('embark build'))
-task('')
+
+task('embark', () => {
+  const availableNetworks = ['development', 'ropsten']
+  const network = args[3] || 'development'
+
+  if (!availableNetworks.includes(network)) {
+    sig.fatal(`Unrecognized Network: ${chalk.white.bgRed.bold(network)}`)
+    process.exit(0)
+  }
+
+  sig.fav(`Blockchain Network: ${chalk.green.bold(network.toUpperCase())}`)
+  sh.exec('rsync -a embark/plugin.js node_modules/embark-tomo/')
+  sig.complete('Copied custom plugin')
+  sh.exec(`embark run --nobrowser --noserver --nodashboard ${network}`)
+})
 
 
 task('db:migrate', () => {
