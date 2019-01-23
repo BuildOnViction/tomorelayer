a0371861b8a7b414c7c80aeb7c72e6455d3f6c3b
vutran1710 <me@vutr.io>
2019-01-23 15:13:03 +0700

    Add tomo-net

--- embark/contracts.js
+++ embark/contracts.js
@@ -7,11 +7,6 @@ module.exports = {
       port: 8545,
       type: "rpc",
     },
-    dappConnection: [
-      "$WEB3",
-      "ws://localhost:8546",
-      "http://localhost:8545"
-    ],
     gas: "auto",
     contracts: {}
   },
@@ -26,7 +21,21 @@ module.exports = {
       type: "rpc",
       accounts: [
         {
-          privateKey: secrets.OWNER_PRIVATE_KEY,
+          privateKey: secrets.ROPSTEN_OWNER_PRIVATE_KEY,
+        },
+      ]
+    }
+  },
+
+  tomonet: {
+    deployment: {
+      host: secrets.TOMO_ENDPOINT,
+      protocol: 'https',
+      port: false,
+      type: "rpc",
+      accounts: [
+        {
+          privateKey: secrets.TOMO_OWNER_PRIVATE_KEY,
         },
       ]
     }
