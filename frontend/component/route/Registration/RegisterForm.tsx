9a649ace0fff405f822e94b427a5b70183ee1575
vutran1710 <me@vutr.io>
2019-01-29 20:53:16 +0700

    Separate the form

--- frontend/component/route/Registration/RegisterForm.tsx
+++ frontend/component/route/Registration/RegisterForm.tsx
@@ -6,84 +6,15 @@ import { Alert, FormGroup, InputGroup, NumericInput, Intent, Button } from '@blu
 import { Grid } from '@utility'
 import { RELAYER_REGISTRATION } from '@action'
 import { SITE_MAP } from '@constant'
+import SignupForm from './SignupForm'
 
-const SignUpForm = props => {
-  const {
-    isSubmitting,
-    handleChange,
-    handleSubmit,
-    values,
-  } = props
-  return (
-    <React.Fragment>
-      <FormGroup
-        className="col-4"
-        helperText="Your Relayer name..."
-        label="Relayer Name"
-        labelFor="name"
-        labelInfo="(required)"
-      >
-        <InputGroup
-          name="name"
-          type="text"
-          onChange={handleChange}
-        />
-      </FormGroup>
 
-      <FormGroup
-        className="col-4"
-        label="Coinbase Address"
-        labelFor="address"
-        labelInfo="(required)"
-      >
-        <InputGroup
-          name="address"
-          type="text"
-          value={values.address}
-          disabled={!!values.address}
-          onChange={handleChange}
-        />
-      </FormGroup>
-
-      <FormGroup
-        className="col-4"
-        label="Order Rate"
-        labelFor="dex_rate"
-        labelInfo="(required)"
-      >
-        <InputGroup
-          name="dex_rate"
-          type="number"
-          onChange={handleChange}
-        />
-      </FormGroup>
-
-      <FormGroup
-        className="col-4"
-        label="Your Relayer Crest"
-        labelFor="logo"
-        labelInfo="(optional)"
-      >
-        <InputGroup
-          name="logo"
-          type="text"
-          onChange={handleChange}
-        />
-      </FormGroup>
-
-      <div className="col-4">
-        <Button onClick={handleSubmit} intent={Intent.PRIMARY} large>
-          {isSubmitting ? 'Loading' : 'Sign Up'}
-        </Button>
-      </div>
-    </React.Fragment>
-  )
-}
 
 class RegisterForm extends React.Component {
 
   state = {
     alert: null,
+    loading: false,
   }
 
   registerNewRelayer = values => this.props.registerRelayer(
@@ -92,7 +23,7 @@ class RegisterForm extends React.Component {
       const errorAlert = err => `Error: ${err.message} (${err.detail})`
       const successAlert = obj => `Success: ${JSON.stringify(obj)}`
       const alert = error ? errorAlert(error) : successAlert(payload)
-      return this.setState({ alert })
+      return this.setState({ alert, loading: false })
     },
   )
 
@@ -116,8 +47,9 @@ class RegisterForm extends React.Component {
           initialValues={{ address }}
           validate={this.validate}
           onSubmit={this.registerNewRelayer}
-          render={SignUpForm}
+          render={SignupForm}
           ref={el => {this.form = el}}
+          loading={this.state.loading}
         />
         <Alert
           canEscapeKeyCancel
