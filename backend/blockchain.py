c2d12cbb829ce801bf17c60f5dac0691393194b6
vutran1710 <me@vutr.io>
2019-01-29 10:16:21 +0700

    no need checking manual registration at backend side

--- backend/blockchain.py
+++ backend/blockchain.py
@@ -11,45 +11,13 @@ is_production = os.getenv('STG') == 'production'
 class Blockchain:
 
     web3 = None
-    contracts = {}
-    relayer_contract = None
 
     def __init__(self):
         """ Interact with Blockchain through SmartContract & WebSocket
         """
         self.web3 = Web3()
-        self.read_local_contracts()
-        self.web3.eth.defaultAccount = self.web3.eth.accounts[0]
 
         if not is_production:
             self.web3.middleware_stack.inject(geth_poa_middleware, layer=0)
 
         logger.warn('Connection status: {}'.format(self.web3.isConnected()))
-
-    def read_local_contracts(self):
-        """ READ JSON FILES OF COMPILED CONTRACTS
-        """
-        try:
-            contract_files = os.listdir('build/contracts')
-            for file_name in contract_files:
-                file = 'build/contracts/' + file_name
-
-                with open(file) as json_file:
-                    contract_dict = json.load(json_file)
-                    contract_name = contract_dict['className']
-                    self.contracts[contract_name] = contract_dict
-                    tornado.autoreload.watch(file)
-
-            registration_contract = os.getenv('CONTRACT_REGISTRATION_NAME')
-            abi = self.contracts[registration_contract]['abiDefinition']
-            address = self.contracts[registration_contract]['deployedAddress']
-            self.relayer_contract = self.web3.eth.contract(address=address, abi=abi)
-
-            logger.info('Loaded all the contract data')
-            logger.info('Watching contract json files for changes')
-            tornado.autoreload.add_reload_hook(lambda: logger.warn('Reloading...'))
-
-        except Exception as err:
-            logger.error(err)
-            logger.error('Cannot read compiled contract data')
-            logger.error('Make sure you have already compiled all the necessary contracts with Embark')
