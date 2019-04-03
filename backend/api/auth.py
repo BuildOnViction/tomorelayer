6d41699406988ba5b2e89a5ca4e83062f84e82ba
vutran1710 <me@vutr.io>
2019-04-03 18:33:33 +0700

    Add SSL cert to backend app

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -9,16 +9,20 @@ class AuthHandler(BaseHandler):
         """
         if self.get_argument('qr_code', None):
             agent_query = self.get_argument('qr_code')
-            qr = self.generate_qr_code(agent_query)
+            qr = self.generate_qr_code()
             return self.json_response(qr)
 
-    def generate_qr_code(self, agent):
+    def post(self):
+        """Receiving request from TomoWallet"""
+        print(self.request_body)
+        breakpoint()
+
+    def generate_qr_code(self):
         from datetime import datetime
         from uuid import uuid4
         message = '[Relayer {}] Login'.format(datetime.now().strftime('%x %H-%M-%S'))
         identity = str(uuid4())
-        qrcode = '{agent}://{base_url}/api/auth?verifyId={identity}'.format(
-            agent=agent,
+        qrcode = '{base_url}/api/auth?verifyId={identity}'.format(
             base_url=base_url,
             identity=identity,
         )
--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
+import os
 from tornado.platform.asyncio import AsyncIOMainLoop
+from tornado import httpserver
 from tornado.ioloop import IOLoop
 from tornado.web import Application
 from tornado.options import options
@@ -14,8 +16,16 @@ define("port", default=settings['port'], help="app port", type=int)
 if __name__ == "__main__":
     parse_command_line()
     AsyncIOMainLoop().install()
-    app = Application(route, default_handler_class=NotFoundHandler, **settings)
-    app.listen(options.port)
+    current_dir = os.getcwd()
+
+    ssl_options = {
+        "certfile": os.path.join(current_dir, "backend/cert/localhost.crt"),
+        "keyfile": os.path.join(current_dir, "backend/cert/localhost.key"),
+    }
+
+    app = Application(route, default_handler_class=NotFoundHandler, ssl_options=ssl_options, **settings)
     app.objects = settings['objects']
     app.blockchain = Blockchain()
+    http_server = httpserver.HTTPServer(app, ssl_options=ssl_options)
+    http_server.listen(options.port)
     IOLoop.current().start()
