6b72f82d5374c97f2ebdce02084815c3ad7e8d99
vutran1710 <me@vutr.io>
2019-07-09 19:07:22 +0700

    fix progress bar

--- src/component/route/Register/ProgressBar.jsx
+++ src/component/route/Register/ProgressBar.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
+import cx from 'classnames'
 import { Box } from '@material-ui/core'
 
-const FulfilledStep = ({ step }) => (
-  <Box>
-    <Box className="step_circle step_circle__2">
-      <Box className="step_circle step_circle__1">
-        <Box className="step_circle step_circle__0">
+const ProgressStep = ({ step, unfulfilled }) => {
+  const classes = (level, bol) => cx(
+    'step_circle',
+    `step_circle__${level}`,
+    {
+      unfulfilled: bol,
+    }
+  )
+  return (
+    <Box className={classes(2, unfulfilled)}>
+      <Box className={classes(1, unfulfilled)}>
+        <Box className={classes(0, unfulfilled)}>
           {step}
         </Box>
       </Box>
     </Box>
-  </Box>
-)
-
-const UnfulfilledStep = ({ step }) => (
-  <Box className="step_circle step_circle__unfulfilled">
-    {step}
-  </Box>
-)
+  )
+}
 
 const ProgressBar = ({ step }) => {
   return (
     <Box display="flex" justifyContent="space-between" alignItems="center" className="mb-3">
-      {[1,2,3,4].map(_step => step >= _step ? <FulfilledStep key={_step} step={_step} /> : <UnfulfilledStep key={_step} step={_step}/>)}
+      {[1,2,3,4].map(_step => (
+        <React.Fragment key={_step} >
+          {step >= _step ? <ProgressStep step={_step} /> : <ProgressStep step={_step} unfulfilled />}
+          {_step < 4 && <div className="progress-trailing" />}
+        </React.Fragment>
+      ))}
     </Box>
   )
 }
