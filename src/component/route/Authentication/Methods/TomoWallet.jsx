7c91acec71b9d6b949b332d0743485a10af086c9
xiemnguyen <xiem.bg@gmail.com>
2019-07-10 15:32:56 +0700

    styling authen

--- src/component/route/Authentication/Methods/TomoWallet.jsx
+++ src/component/route/Authentication/Methods/TomoWallet.jsx
@@ -1,62 +1,17 @@
 import React from 'react'
 import { QRCode } from 'react-qr-svg'
-// import * as blk from 'service/blockchain'
 import {
   Container,
-  Grid,
   Typography,
+  Box,
+  Grid,
 } from '@material-ui/core'
-// import success_icon from 'asset/success.png'
-import appstore from 'asset/appstore-logo.png'
-import googleplay from 'asset/google-play-logo.png'
-
-
-/*
- * const SuccessLogin = ({ address, balance, confirm }) => (
- *   <Grid className="mt-2 justify-start align-center">
- *     <img alt="success" src={success_icon} width="60" height="60" className="block p-1" />
- *     <div className="p-1 font-2 mr-2">
- *       <div>
- *         <b>Address: </b>
- *         <span>{address}</span>
- *       </div>
- *       <div>
- *         <b>Balance: </b>
- *         <span>{balance} TOMO</span>
- *       </div>
- *     </div>
- *     <button className="btn btn-unlock" onClick={confirm}>
- *       Use this account!
- *     </button>
- *   </Grid>
- * ) */
+import { CustomLink } from 'component/shared/Adapters'
 
 const PendingLogin = ({ qrcode }) => (
-  <Container maxWidth="md">
-    <Grid container spacing={4} justify="center" alignItems="center" className="p-2">
-
-      <Grid item sm={12} md={7} container justify="center">
-        <Typography component="h4">
-          Scan QR code using TomoWallet to unlock
-        </Typography>
-        <Typography component="div" className="mt-1 mb-1">
-          Haven’t installed TomoWallet yet? Download below
-        </Typography>
-        <Grid item container justify="center" spacing={2}>
-          <Grid item sm={6} md={4}>
-            <a href="https://goo.gl/MvE1GV">
-              <img alt="" src={appstore} height="40" />
-            </a>
-          </Grid>
-          <Grid item sm={6} md={4}>
-            <a href="https://goo.gl/4tFQzY">
-              <img alt="" src={googleplay} height="40" />
-            </a>
-          </Grid>
-        </Grid>
-      </Grid>
-
-      <Grid sm={12} md={3} container justify="center" item>
+  <Container maxWidth="xs">
+    <Grid container direction="column" spacing={3}>
+      <Grid item container justify="center">
         <QRCode
           bgColor="#FFFFFF"
           fgColor="#000000"
@@ -65,7 +20,13 @@ const PendingLogin = ({ qrcode }) => (
           value={qrcode}
         />
       </Grid>
-
+      <Grid item container justify="center">
+        <Typography variant="body1">Scan QR code using TomoWallet to unlock</Typography>
+        <Box>
+          Haven’t installed TomoWallet yet?
+          <CustomLink to="/"> Click here</CustomLink>
+        </Box>
+      </Grid>
     </Grid>
   </Container>
 )
--- src/component/shared/Adapters.jsx
+++ src/component/shared/Adapters.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import { Link as MuiLink } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
 
 export const AdapterLink = React.forwardRef((props, ref) => <Link innerRef={ref} {...props} />)
+
+const StyledLink = withStyles(theme => ({
+    root: {
+        color: '#577eef'
+    }
+}))(MuiLink)
+
+export const CustomLink = props => (
+    <StyledLink component={AdapterLink} {...props} />
+)
