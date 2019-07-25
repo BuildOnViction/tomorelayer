375d485fc021eb6b04c0026034f0bb7612ffcd75
vutran1710 <me@vutr.io>
2019-07-25 13:50:12 +0700

    theme switcher

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { MuiThemeProvider } from '@material-ui/core/styles'
-import { Provider } from 'redux-zero/react'
+import { Provider, connect } from 'redux-zero/react'
 import App from './component/App'
 import store from './service/store'
-import theme from './theme.config'
+import theme, { availableThemes } from './theme.config'
+
+const mapTheme = state => ({
+  activeTheme: state.activeTheme
+})
+
+const BootstrappedApp = connect(mapTheme)(({ activeTheme }) => (
+  <MuiThemeProvider theme={theme(availableThemes[activeTheme])}>
+    <App />
+  </MuiThemeProvider>
+))
 
 ReactDOM.render((
-  <MuiThemeProvider theme={theme}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <BootstrappedApp />
+  </Provider>
 ), document.getElementById('root'))
