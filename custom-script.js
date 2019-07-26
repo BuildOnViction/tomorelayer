214e2d1a9023e304170de40fe38141477ce3be19
vutran1710 <me@vutr.io>
2019-07-26 16:18:28 +0700

    revert script

--- custom-script.js
+++ custom-script.js
@@ -13,25 +13,23 @@ const port = process.env.NODE_ENV === 'test' ? 8889 : 8888
 
 
 killPort(port).then(() => {
-  execSh(`ENV_PATH=${envfile} pipenv run python ./backend/app.py`)
-  /*
-   *   request('https://localtunnel.me', (err, res) => {
-   *     const status = res.statusCode
-   *
-   *     if (status <400) {
-   *       localtunnel(port, (err, tunnel) => {
-   *         if (err) {
-   *           return console.error(err)
-   *         }
-   *         const baseUrl = tunnel.url
-   *         console.info(baseUrl)
-   *         execSh(`TUNNEL_URL=${baseUrl} ENV_PATH=${envfile} pipenv run python ./backend/app.py`)
-   *       })
-   *     } else {
-   *       console.log('LocalTunnel is Offline, run local app')
-   *       execSh(`ENV_PATH=${envfile} pipenv run python ./backend/app.py`)
-   *     }
-   *   })
-   */
+
+  request('https://localtunnel.me', (err, res) => {
+    const status = res.statusCode
+
+    if (status <400) {
+      localtunnel(port, (err, tunnel) => {
+        if (err) {
+          return console.error(err)
+        }
+        const baseUrl = tunnel.url
+        console.info(baseUrl)
+        execSh(`TUNNEL_URL=${baseUrl} ENV_PATH=${envfile} pipenv run python ./backend/app.py`)
+      })
+    } else {
+      console.log('LocalTunnel is Offline, run local app')
+      execSh(`ENV_PATH=${envfile} pipenv run python ./backend/app.py`)
+    }
+  })
 
 })
