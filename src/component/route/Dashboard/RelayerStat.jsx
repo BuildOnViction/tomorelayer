33c54af96010de0e9b7de72c36e36379e013ecbc
vutran1710 <me@vutr.io>
2019-07-28 14:03:31 +0700

    Add relayerlogo

--- src/component/route/Dashboard/RelayerStat.jsx
+++ src/component/route/Dashboard/RelayerStat.jsx
@@ -1,13 +1,56 @@
 import React from 'react'
+import {
+  Avatar,
+  Box,
+  Grid,
+  Typography,
+} from '@material-ui/core'
+import { withStyles } from '@material-ui/styles'
+import { StyledLink } from 'component/shared/Adapters'
 
-const RelayerHome = ({ relayers, match }) => {
-  const coinbase = match.params.coinbase
-  const activeRelayer = relayers[coinbase]
-  return (
-    <div>
-      Dashboard of Relayer {activeRelayer.name}
-    </div>
-  )
+const StyledAvatar = withStyles(theme => ({
+  root: {
+    height: 60,
+    width: 60,
+    borderRadius: '50%',
+    margin: 20,
+  }
+}))(Avatar)
+
+
+export default class RelayerStat extends React.Component {
+
+  render() {
+    const {
+      relayers: allRelayers,
+      match,
+    } = this.props
+
+    const coinbase = match.params.coinbase
+    const relayer = allRelayers[coinbase]
+
+    return (
+      <Grid container direction="column">
+        <Grid item>
+          <Box display="flex" alignItems="center">
+            <Box>
+              <StyledAvatar src={relayer.logo} alt={relayer.name} />
+            </Box>
+            <Box display="flex" flexDirection="column">
+              <Box>
+                <Typography variant="h6">
+                  {relayer.name}
+                </Typography>
+              </Box>
+              <Box>
+                <StyledLink href={relayer.link} rel="noopener noreferrer" target="_blank">
+                  {relayer.link}
+                </StyledLink>
+              </Box>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    )
+  }
 }
-
-export default RelayerHome
