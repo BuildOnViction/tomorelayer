e2fc48c3c214dadd8afd6d49fca1e324406f92b6
vutran1710 <me@vutr.io>
2019-08-13 14:42:43 +0700

    log chain

--- backend/blockchain.py
+++ backend/blockchain.py
@@ -13,5 +13,6 @@ class Blockchain:
         """ Interact with Blockchain through SmartContract & WebSocket
         """
         self.web3 = w3
-        logger.info('Connection status: %s', self.web3.isConnected())
+        logger.info('WEB3_PROVIDER_URI = %s', os.getenv('WEB3_PROVIDER_URI'))
+        logger.info('Chain connection status: %s', self.web3.isConnected())
         logger.info('Connection name: %s', self.web3.clientVersion)
