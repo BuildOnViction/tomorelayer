533624e069d26bacb2c872cb61c6d5ac0cf89a00
thanhvk <thanhvk.w3s@gmail.com>
2019-08-14 14:30:04 +0700

    Responsive header

--- src/component/shared/PageHeader.jsx
+++ src/component/shared/PageHeader.jsx
@@ -3,13 +3,15 @@ import { connect } from 'redux-zero/react'
 import SearchIcon from '@material-ui/icons/Search'
 import {
   Box,
-  Button,
   Container,
   Grid,
   InputAdornment,
   Link,
   // Switch,
   TextField,
+  Hidden,
+  withWidth,
+  withStyles,
 } from '@material-ui/core'
 import {
   isEmpty,
@@ -17,11 +19,23 @@ import {
 import {
   UserMenu,
   RelayerMenu,
+  StartRelayerButton,
+  ResponsiveMenu,
 } from './Menus'
 import { AdapterLink } from './Adapters'
 import logo from 'asset/logo-tomorelayer.svg'
 
+const ref = React.createRef()
 
+const SearchTextField = withStyles(theme => ({
+  root: {
+    [theme.breakpoints.down('sm')]: {
+      '& .MuiOutlinedInput-root': {
+        backgroundColor: theme.palette.document,
+      },
+    },
+  },
+}))(TextField)
 class PageHeader extends React.Component {
 
   render() {
@@ -40,30 +54,27 @@ class PageHeader extends React.Component {
       <Box className="tomo-header">
         <Container maxWidth="lg" className="p-1">
           <Grid container direction="row" justify="space-between" alignItems="center">
-            <Grid item sm={3} md={2}>
+            <Grid item xs={3} md={2}>
               <img alt="logo" src={logo} height="30px" />
               {/* <Link display="block" component={AdapterLink} to="/">
                   <img alt="logo" src={logo} height="30px" />
                   </Link> */}
             </Grid>
-            <Grid item sm={false} md={6}>
-              <TextField
-                placeholder="Search everything you want…"
-                fullWidth
-                variant="outlined"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <SearchIcon />
-                    </InputAdornment>
-                  )
-                }}
-              />
-            </Grid>
-            <Grid item sm={6} md={4} container justify="flex-end" direction="row" spacing={4} alignItems="center">
-              {auth && userOwnRelayer && <RelayerMenu relayers={relayers} />}
-              {auth && !userOwnRelayer && <Button variant="contained" component={AdapterLink} to="/register">Start a Relayer</Button>}
-              {auth && <UserMenu />}
+            <Hidden smDown>
+              <Grid item xs={6} md={6}>
+                <SearchBox />
+              </Grid>
+            </Hidden>
+            <Grid item xs={9} md={4} container justify="flex-end" direction="row" spacing={4} alignItems="center">
+              <Hidden smDown>
+                {auth && userOwnRelayer && <RelayerMenu relayers={relayers} />}              
+                {auth && !userOwnRelayer && <StartRelayerButton />}
+                {auth && <UserMenu />}
+              </Hidden>
+
+              <Hidden mdUp>
+                {auth && userOwnRelayer && <ResponsiveMenu ref={ref} relayers={relayers} userOwnRelayer={userOwnRelayer}><SearchBox /></ResponsiveMenu>}
+              </Hidden>  
               {!auth && <Link component={AdapterLink} to="/login" className="ml-3">Help</Link>}
               {/* <Switch checked={activeTheme === 'dark'} onChange={() => changeTheme()} /> */}
             </Grid>
@@ -73,6 +84,20 @@ class PageHeader extends React.Component {
     )
   }
 }
+
+const SearchBox = () => (
+  <SearchTextField
+    placeholder="Search everything you want…"
+    fullWidth
+    variant="outlined"
+    InputProps={{
+      endAdornment: (
+        <InputAdornment position="end">
+          <SearchIcon />
+        </InputAdornment>
+      )
+    }}
+  />)
 
 const mapProps = state => ({
   activeTheme: state.activeTheme,
@@ -84,4 +109,6 @@ const actions = {
   }),
 }
 
-export default connect(mapProps, actions)(PageHeader)
+const PageHeaderResponsive = withWidth()(PageHeader)
+
+export default connect(mapProps, actions)(PageHeaderResponsive)
