b57765b2994dabd7442fa016ae6c966d102c3066
vutran1710 <me@vutr.io>
2019-08-27 14:40:43 +0700

    relayer link must be uniquer

--- backend/model.py
+++ backend/model.py
@@ -39,7 +39,7 @@ class Relayer(PwModel):
     from_tokens = ArrayField(pw.CharField, default=[])
     to_tokens = ArrayField(pw.CharField, default=[])
     logo = pw.CharField(max_length=200, null=True)
-    link = pw.CharField(max_length=255, null=True)
+    link = pw.CharField(max_length=255, null=True, unique=True)
     resigning = pw.BooleanField(default=False)
     lock_time = pw.IntegerField(null=True)
 
