a7bd284ab783b8dd792edbc725c0453680c63e15
vutran1710 <me@vutr.io>
2019-09-13 17:45:57 +0700

    update pouch on update backend

--- src/component/route/Dashboard/actions.js
+++ src/component/route/Dashboard/actions.js
@@ -1,11 +1,32 @@
 import { getDexTrades } from 'service/backend'
 
-export const UpdateRelayer = (state, relayer) => {
+export const UpdateRelayer = async (state, relayer) => {
   const Relayers = Array.from(state.Relayers)
   const index = Relayers.findIndex((r) => r.id === relayer.id)
   Relayers[index] = relayer
 
+  const user = {
+    ...state.user,
+    relayers: {
+      ...state.user.relayers,
+      [relayer.coinbase]: relayer,
+    },
+  }
+
+  const pouch = state.pouch
+  await pouch.get('relayer' + relayer.id.toString()).then(doc => pouch.put({
+    ...doc,
+    ...relayer,
+    fuzzy: [
+      relayer.name,
+      relayer.owner,
+      relayer.coinbase,
+      relayer.address,
+    ].join(','),
+  }))
+
   return {
+    user,
     Relayers,
   }
 }
