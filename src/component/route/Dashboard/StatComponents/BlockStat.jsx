e00a65b3eb8ddcfd51f0408230b48b76884afea0
vutran1710 <me@vutr.io>
2019-10-22 22:11:08 +0700

    hightlight changes

--- src/component/route/Dashboard/StatComponents/BlockStat.jsx
+++ src/component/route/Dashboard/StatComponents/BlockStat.jsx
@@ -1,29 +1,53 @@
 import React from 'react'
+import cx from 'classnames'
 import { Box, Grid, Paper, Typography } from '@material-ui/core'
 import networkFeeIcon from 'asset/icon-network-fees.png'
 import networkVolIcon from 'asset/icon-network-volume.png'
 import tradeIcon from 'asset/icon-trades.png'
 import tomoPriceIcon from 'asset/icon-tomo-price.png'
 
-const StatCard = ({
-  icon,
-  stat,
-  helpText,
-}) => (
-  <Grid item xs={6} md={3}>
-    <Paper elevation={0} style={{ padding: 20 }}>
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <img alt="" src={icon} height="27px" />
-        <Typography variant="h6" className="mt-1">
-          {stat}
-        </Typography>
-        <Typography variant="body2" className="text-center m-0">
-          {helpText}
-        </Typography>
-      </Box>
-    </Paper>
-  </Grid>
-)
+class StatCard extends React.Component {
+  state = {
+    hightlight: false,
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.stat !== this.props.stat) {
+      this.setState({ hightlight: true })
+      setTimeout(() => this.setState({ hightlight: false }), 1000)
+    }
+  }
+
+  render() {
+    const {
+      hightlight
+    } = this.state
+
+    const {
+      icon,
+      stat,
+      helpText,
+    } = this.props
+
+    const cls = cx('mt-1', {'stat-hightlight': hightlight })
+
+    return (
+      <Grid item xs={6} md={3}>
+        <Paper elevation={0} style={{ padding: 20 }}>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <img alt="" src={icon} height="27px" />
+            <Typography variant="h6" className={cls}>
+              {stat}
+            </Typography>
+            <Typography variant="body2" className="text-center m-0">
+              {helpText}
+            </Typography>
+          </Box>
+        </Paper>
+      </Grid>
+    )
+  }
+}
 
 const BlockStat = ({ data }) => (
   <Grid item container spacing={4}>
