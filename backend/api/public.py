806076cad057c403a53c49d2819042c486b6a33c
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-11-18 11:37:24 +0700

    fix issue cache by remove cache

--- backend/api/public.py
+++ backend/api/public.py
@@ -8,13 +8,13 @@ from .base import BaseHandler
 class PublicHandler(BaseHandler):
 
     async def get(self):
-        redis = self.application.redis
-        keys = await redis.hlen('public_res')
-        logger.debug('Length = %d', keys)
+        # redis = self.application.redis
+        # keys = await redis.hlen('public_res')
+        # logger.debug('Length = %d', keys)
 
-        if keys:
-            cached = await redis.hgetall('public_res', encoding='utf-8')
-            return self.json_response({k: json.loads(v) for k, v in cached.items()})
+        # if keys:
+        #     cached = await redis.hgetall('public_res', encoding='utf-8')
+        #     return self.json_response({k: json.loads(v) for k, v in cached.items()})
 
         relayers = [model_to_dict(relayer or {}) for relayer in Relayer.select()]
         contracts = [model_to_dict(c or {}) for c in Contract.select().where(Contract.obsolete == False)]
@@ -24,7 +24,7 @@ class PublicHandler(BaseHandler):
             'Contracts': contracts,
             'Tokens': tokens
         })
-        await redis.hmset_dict('public_res',
-                               Relayers=json.dumps(relayers),
-                               Contracts=json.dumps(contracts),
-                               Tokens=json.dumps(tokens))
+        # await redis.hmset_dict('public_res',
+        #                        Relayers=json.dumps(relayers),
+        #                        Contracts=json.dumps(contracts),
+        #                        Tokens=json.dumps(tokens))
