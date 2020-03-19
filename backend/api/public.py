2c90751e11dd34dce7dd6bf7182993569e090161
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-03-19 09:32:46 +0000

    abi from file

--- backend/api/public.py
+++ backend/api/public.py
@@ -12,11 +12,14 @@ class PublicHandler(BaseHandler):
         relayers = [model_to_dict(relayer or {}) for relayer in Relayer.select()]
         contracts = [model_to_dict(c or {}) for c in Contract.select().where(Contract.obsolete == False)]
         tokens = [model_to_dict(token or {}) for token in Token.select()]
+        for contract in contracts:
+            if contract['name'] == 'RelayerRegistration':
+                contract['abi'] = Blockchain.RegistrationABI
+            if contract['name'] == 'TOMOXListing':
+                contract['abi'] = Blockchain.TOMOXListingABI
 
         self.json_response({
             'Relayers': relayers,
             'Contracts': contracts,
             'Tokens': tokens
-            'RegistrationABI': Blockchain.RegistrationABI
-            'TOMOXListingABI': Blockchain.TOMOXListingABI
         })
