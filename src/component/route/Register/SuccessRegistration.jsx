5d4cdcc00fcf268d36a7a87df238af7e82dd79f4
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-04-22 09:07:39 +0000

    fix link

--- src/component/route/Register/SuccessRegistration.jsx
+++ src/component/route/Register/SuccessRegistration.jsx
@@ -13,7 +13,7 @@ const SuccessRegistration = ({ navigate, deposit, link }) => (
       </Typography>
       <Typography variant="body1" style={{ lineHeight: 1.5 }}>
         You’ve succesfully deposited {deposit} TOMO to SmartContract.<br />
-        Your DEX is temporarily running at <StyledLink href="{link}" rel="noopener noreferrer" target="_blank">{link}</StyledLink>.
+        Your DEX is temporarily running at <StyledLink href={ link } rel="noopener noreferrer" target="_blank">{link}</StyledLink>.
       </Typography>
       <Typography component="div" variant="body2" style={{ lineHeight: 1.3 }}>
         To launch your own DEX using TomoX-SDK, <br /> follow this <StyledLink href="https://docs.tomochain.com/masternode/tomox-sdk/" rel="noopener noreferrer" target="_blank">link</StyledLink> for detailed instructions
